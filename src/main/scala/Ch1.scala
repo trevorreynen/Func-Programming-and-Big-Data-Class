ec94f06b31a85c7641670edfa3e0dc4c4227cc06
Trevor Reynen <trevorreynen@gmail.com>
2022-06-06 18:44:34 -0400

    Fixed comments in Ch1.scala, added Ch2.scala

--- src/main/scala/Ch1.scala
+++ src/main/scala/Ch1.scala
@@ -1,56 +1,61 @@
 // Ch1.scala
 
-/**
- * Created by Wei Zhong on 07/07/2020.
- * Chapter 1 will introduce Scala and Functional Programming basics.
-**/
+// CSCI-509 - Functional Programming & Big Data | USC Upstate
+// Due: Mon. 06/06/2022
+// Trevor Reynen
 
+// Created by Wei Zhong on 07/07/2020.
+// Chapter 1 will introduce Scala and Functional Programming basics.
 object Ch1 {
-
     // Main function is where you start your application.
     def main(args: Array[String]): Unit = {
-
-        // Standard way to create Scala variable. You do not need to specify type of
-        // variable. Type information is inferred by compiler.
+        // Standard way to create Scala variable.
+        // You do not need to specify type of variable.
+        // Type information is inferred by compiler.
         val answer = 8 * 5 + 2
 
         // If you want to be more defensive, you can give the type information.
         // However, this is rarely used.
         val answer1: Int = 8 * 5 + 2
 
-        // A value declared with val is actually a constant. You can not change it's
-        // contents. In other words, answer is immutable. Answer = 10
+        // A value declared with 'val' is actually a constant. You can not change it's contents.
+        // In other words, answer is immutable.
 
-        // To declare a variable whose contents can vary, use var.
+        // To declare a variable whose contents can vary, use 'var'.
         var counter = 0
         counter = 1 // Ok to change a var.
 
-        // In other words, var is mutable variable. Val is immutable variable.
+        // In other words, 'var' is a mutable variable. 'val' is a immutable variable.
         println("Counter: " + counter)
+        // Output: Counter: 1
 
-        // In Scala, you are encouraged to use a val unless you really need to
-        // change contents of a variable. In other words, immutability is encouraged.
 
-        // You can pecify the type of the variable if necessary.
+        // In Scala, you are encouraged to use 'val' unless you really need to change contents of a
+        // variable. In other words, immutability is encouraged.
+
+
+        // You can specify the type of the variable if necessary.
         val greeting: String = "Hello"
 
-        // 1.3 Commonly Used Type
-        // Unlike Java, all types of variables are classes. In other words, Scala
-        // is purely a object-oriented language.
+
+
+        // ========== 1.3 Commonly Used Type ==========
+        // Unlike Java, all types of variables are classes.
+        // In other words, Scala is purely an object-oriented language.
         val res0 = 1.toString
         println("res0: " + res0)
-        println()
+        // Output: res0: 1
 
 
-        // This is declarative style: tell you what I want to do.
-        // Here, I want to get the number from 1 to 10.
+        // Declarative programming involves stating what the task or desired outcome is.
+        // Here, we want to get the numbers from 1 to 10.
         // Range is one type of collection.
-        // Int 1 is implicitly converted to RichInt, which has more functionality
-        // than Int class.
+        // Int 1 is implicitly converted to RichInt, which has more functionality than Int class.
         val res1 = 1.to(10)
 
         // Access each element of range and print it out.
         res1.foreach(println)
+        // Output: 1 2 3 4 5 6 7 8 9 10    (each number on a new line)
 
         // Calculate intersection between two Strings.
         val res2 = "Hello".intersect("World")
@@ -58,9 +63,10 @@ object Ch1 {
         // Output: res2: lo
 
 
-        // 1.4 Arithmetic and Operator Overloading.
+
+        // ========== 1.4 Arithmetic and Operator Overloading ==========
         // In general, (a method b) is a shorthand for (a.method(b))
-        // For example. 1.to(10) is same as 1 to 10 (infix notation)
+        // For example, 1.to(10) is same as 1 to 10 (infix notation)
 
         val res3 = 1.to(10)
 
@@ -70,7 +76,7 @@ object Ch1 {
 
         // a + b is shorthand for a.+(b).
         // + in Scala is the method name.
-        // In other words, Scala has no silly predudice against non-alphanumeric
+        // In other words, Scala has no silly prejudice against non-alphanumeric
         // characters in method names.
 
         val res5 = "hello".intersect("World")
@@ -78,29 +84,31 @@ object Ch1 {
         val res6 = "hello" intersect "World" // Infix notation.
 
         var counter2 = 0
-        counter2 += 1 // Increate counter - Scala has no ++ operator.
+        counter2 += 1 // Increase counter - Scala has no ++ operator.
 
 
         // BigInt is used to store very large integer.
         // You can use the usual mathematical operator with BigInt objects.
 
-        val x: BigInt = 123456789
+        val x: BigInt = 1234567890
         val res7 = x * x * x
         println("res7: " + res7)
-
-        // This is much better than Java, where you would have to call
-        // x.multiply(x).multiply(x) because * is not a method in Java.
-        // This is very verboce.
+        // Output: res7: 1881676371789154860897069000
 
 
-        // 1.5 Calling Functions and Methods
+        // This is much better than Java, where you would have to call x.multiply(x).multiply(x)
+        // because * is not a method in Java. This is very verbose.
+
+
+
+        // ========== 1.5 Calling Functions and Methods ==========
         // In Scala, _ character is a wildcard like * in Java.
-        // We are importing all classes in scala.math package.
+        // We are importing all classes in the scala.math package.
         import scala.math._
 
         val res8 = sqrt(2)
         println("res8: " + res8)
-        // Output: res8: 1.4142135623
+        // Output: res8: 1.4142135623730951
 
         val res9 = pow(2, 4)
         println("res9: " + res9)
@@ -109,21 +117,21 @@ object Ch1 {
         // Scala method without parameters often do not use parenthesis.
         val res10 = "hello".distinct
         println("res10: " + res10)
-        // Output: res10: hello
+        // Output: res10: helo
 
 
-        // In Scala, semicolon is optional at the end of statement since we want
-        // to save some typing.
+        // In Scala, semicolon is optional at the end of a statement
+        // since we want to save some typing.
 
 
-        // A class in Scala has companion object whose methods act just like
-        // static methods do in Java.
-        // BigInt companion object to the BigInt class has a method
-        // called probablePrime.
+        // A class in Scala has companion object whose methods act just like static
+        // methods do in Java.
+        // BigInt companion object to the BigInt class has a method called probablePrime.
 
         // Produce random prime with 100 bit long.
         val res11 = BigInt.probablePrime(100, scala.util.Random)
         println("res11: " + res11)
+        // Output: res11: 673508361218937640397377745251
 
         // It yields a new BigInt object, without having to use new.
         val res12 = BigInt("1234567890")
@@ -134,6 +142,7 @@ object Ch1 {
         // Why without new is good?
         val res14 = BigInt("1234567890") * BigInt("11235811321")
         println("res14: " + res14)
+        // Output: res14: 13871371875005082690
 
         // Using the apply method of a companion object is common Scala idiom
         // for constructing objects.
@@ -145,8 +154,7 @@ object Ch1 {
         // Output: res15: 1, 4, 9, 16
 
         // Recap
-        // In this section, we introduce some of the basic concepts
-        // of Scala programming.
+        // In this section, we introduce some of the basic concepts of Scala programming.
     }
 }
 
---
+++ src/main/scala/Ch2.scala
@@ -0,0 +1,318 @@
+// Ch2.scala
+
+// CSCI-509 - Functional Programming & Big Data | USC Upstate
+// Due: Tue. 06/07/2022
+// Trevor Reynen
+
+// Created by Wei Zhong on 06/28/2020.
+
+
+// In this chapter, we will discuss control structure and functions.
+
+// expresions vs. statement
+// An expression produces a value.
+// A statement carries out an action. Action can usually have side effects (mutation).
+
+// In Scala, almost all constructs have values.
+// This is major features of functional programming.
+// This feature can make a program more concise and easier to read.
+object Ch2 {
+    def main(args: Array[String]): Unit = {
+        // ========== 2.1 Conditional Expressions ==========
+        // In Scala, if/else produces a value.
+
+        val x = 0
+
+        // Type of s is inferred. So you know it's a type during compiling time.
+        // Scala is static typing like Java. Developers do not write
+        // the type of variable out. Your code is more concise.
+        val s = if (x > 0) 1.0 else -1.0
+        println("s: " + s)
+        // Output: s: -1.0
+
+
+        // Type of a mixed expression is the common supertype of both branches.
+        // Any is equivalent to Object. Common supertype of String and Int is Any.
+
+        val res1 = if (x > 0) "positive" else -1
+
+
+
+        // ========== 2.2 Statements ==========
+        var n = 10  // n is mutable.
+        var r = 1
+        if (n > 0) {
+            // This is called a statement. Statement usually has a side effect.
+            // Side effect means that you mutate r.
+            r = r * n
+            n -= 1
+        }
+
+
+
+        // ========== 2.3 Block Expressions and Assignments ==========
+        // In Scala, a block {} produces a value.
+        // The value of the block is the value of the last expression.
+        // In Scala, return expression is rarely used.
+
+        val x0 = 1.0
+        val y0 = 1.0
+        val x1 = 4.0
+        val y1 = 5.0
+
+        import scala.math._
+
+        val distance = {
+            val dx = x1 - x0
+            val dy = y1 - y0
+            // The last expression is value of block.
+            sqrt(dx * dx + dy * dy)
+        }
+
+        println("distance: " + distance)
+        // Output: distance: 5.0
+
+
+
+        // ========== 2.5 Loops ==========
+        // r and n are mutable variables, which is declared previously.
+        r = 1
+        n = 10
+
+        while (n > 0) {
+            // This statement has side effects: you are mutating r.
+            r = r * n
+            // n is counter. Mutating n.
+            n -= 1
+        }
+
+        println("r: " + r + " n: " + n)
+        // Output: r: 3628800 n: 0
+
+
+        // For any language features, you need to evaluate it's advantages and disadvantages.
+        // As a developer, you need to have the critical mindset.
+
+        // For Expressions, the construct:
+        // for (i <- expr)
+        // makes i traverse all values of expr on the right.
+
+        r = 1
+        n = 10
+        // The type of local variable i is the element type of collection
+        // Value of i can be 1, 2, ..., 9, 10
+        for (i <- 1 to n)
+            r = r * i
+
+        println("r: " + r)
+        // Output: r: 3628800
+
+
+        // Other example of for expression
+        val s1 = "Hello"
+        var sum1 = 0
+
+        // Until returns a range that does not include the upperbound.
+        for (i <- 0 until s1.length)
+            sum1 += s1(i)
+
+        println("sum1: " + sum1)
+        // Output: sum1: 500
+
+
+        // For expression is the feature of functional programming. i is not mutated.
+        // For every iteration, i is recreated and reinitialized.
+
+        // In this example, there is no need to use indexes.
+        // You can directly loop over character.
+
+        sum1 = 0
+        for (ch <- "Hello") sum1 += ch
+
+
+
+        // ========== 2.6 Advanced For Loops and For Comprehensions ==========
+        // For comprehensions are an important feature of functional language.
+
+        // You can have multiple generators of the form variable <- expression.
+        // You can separate them by semicolons. There is no mutation in this loop.
+        // i and j are never mutated. For each iteration, i and j are recreated.
+
+        // Inner loop is j <- 1 to 4.
+        // Inner loop moves faster than outer loop.
+        for (i <- 1 to 3; j <- 1 to 4)
+            print((10 * i + j) + " ")
+            // Output: 11 12 13 14 21 22 23 24 31 32 33 34
+        println()
+
+        // You can have any number of definition, introducing variable that can be used
+        // inside the loop. Under the hood, this code is translated into functional code
+        // using map and flatmap function.
+        for (i <- 1 to 3; from = 4 - i; j <- from to 3)
+            print((10 * i + j) + " ")
+            // Output: 13 22 23 31 32 33
+        println()
+
+        // Alternative syntax: enclose generators, guard, and definition inside curly braces.
+        // Semicolon will be dropped.
+        for {
+            i <- 1 to 3
+            from = 4 - i
+            j <- from to 3
+        } print((10 * i + j) + " ")
+          // Output: 13 22 23 31 32 33
+        println()
+
+        // When the body of the for loop starts with yield, the loop constructs a collection
+        // of values, one for each iteration.
+
+        // res18 is indexedsequence. Usually we use vector to represent indexedsequence.
+        // Vector is an immutable collection.
+
+        val res18 = for (i <- 1 to 10) yield 1 % 3
+        println("res18: " + res18)
+        // Output: res18: Vector(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+
+
+        // Generated collection is compatible with the first generator.
+        val res19 = for (c <- "Hello"; i <- 0 to 1) yield (c + i).toChar
+        println("res19: " + res19)
+        // Output: res19: HIeflmlmop
+
+        val res20 = for (i <- 0 to 1; c <- "Hello") yield (c + i).toChar
+        println("res20: " + res20)
+        // Output: res20: Vector(H, e, l, l, o, I, f, m, m, p)
+
+
+
+        // ========== 2.7 Functions ==========
+        // public static double abs (equivalent java syntax)
+
+        // Meaning of return in Scala is different from Java.
+        // Return in Scala actually will interrupt the thread.
+        def abs(x: Double): Double = if (x >= 0) x else -x
+
+        println(abs(3))
+        // Output: 3.0
+        println(abs(-3))
+        // Output: 3.0
+
+        // Imperative Style
+        def fac(n: Int): Int = {
+            var r = 1
+            for (i <- 1 to n) r = r * i
+            r
+        }
+
+        println(fac(4))
+        // Output: 24
+
+
+        // For factorial, the functional style needs to use recursion.
+        // Recursive function has no mutation. However, it is costly.
+
+        // The beauty of the recursive function is that you get rid of mutations
+        // inside the function.
+
+        def recursiveFac(n: Int): Int = {
+            if (n <= 0) 1 else n * recursiveFac(n-1)
+        }
+
+        println(recursiveFac(4))
+        // Output: 24
+
+
+
+        // ========== 2.8 Default and Named Arguments ==========
+        // Left and right has default values.
+        def decorate(str: String, left: String="[", right: String="]"): String = {
+            left + str + right
+        }
+
+
+        println(decorate("Hello"))
+        // Output: [Hello]
+
+        // right="]<<<" is called named argument. Named argument can make sure your code is more
+        // readable if you have a large number of parameters.
+        println(decorate(str="Hello", right="]<<<"))
+        // Output: [Hello]<<<
+
+
+
+        // ========== 2.9 Variable Arguments ==========
+        // Sometimes it's convenient to implement a function that takes a variable
+        // number of arguments. Args is argument sequence. Result is the local variable.
+        // If mutation only happens within the local function, that mutation will not affect
+        // other parts of the program. This type of mutation is allowed for semi-functional style.
+        def sum(args: Int*): Int = {
+            var result = 0
+            for (arg <- args) result += arg
+            result
+        }
+
+        val result1 = sum(1, 4, 9, 16, 25)
+        println("result1: " + result1)
+        // Output: result1: 55
+
+        val result2 = sum(1, 4)
+        println("result2: " + result2)
+        // Output: result2: 5
+
+        val result3 = sum()
+        println("result3: " + result3)
+        // Output: result3: 0
+
+        // Tell the compiler that you want the parameter to be considered as argument sequence.
+        val result4 = sum(1 to 5: _*)
+        println("result4: " + result4)
+        // Output: result4: 15
+
+
+
+        // ========== 2.10 Procedure ==========
+        // Procedure is the function with nothing to return.
+        // Unit means that this function does not return anything.
+        // Box will print the border for String.
+
+        def box(s: String): Unit = {
+            val border = "-" * s.length + "--\n"
+            println("\n" + border + "|" + s + "|\n" + border)
+        }
+
+        box("Fred")
+        // Output:
+        // ------
+        // |Fred|
+        // ------
+        box("USC Upstate")
+        // Output:
+        // -------------
+        // |USC Upstate|
+        // -------------
+
+
+
+        // ========== 2.12 Exception ==========
+        // Exception happens when you have some illegal operations.
+        // Exception expression has the special data type called Nothing.
+
+        def root(x: Double): Double = {
+            if (x >= 0)
+                sqrt(x)
+            else
+                throw new IllegalArgumentException("x should not be negative")
+        }
+
+        try {
+            println(root(4))
+            // Output: 2.0
+            println(root(-4))
+            // Output: java.lang.IllegalArgumentException: x should not be negative.
+        } catch {
+            case error: Exception => println(error)
+        }
+
+    }
+}
+
