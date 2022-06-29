3e30105ecab94310342e880d904c328a4e7ccf6a
Trevor Reynen <trevorreynen@gmail.com>
2022-06-28 23:43:43 -0400

    Finished Homework6 due Wed. 6/29

--- src/main/scala/Homework6.scala
+++ src/main/scala/Homework6.scala
@@ -7,25 +7,22 @@
 // ==========<  Homework 6  >==========
 // Count up how many of each star rating exists in the MovieLens 100k data set.
 
-// Create a case class with Schema of u.data.
-// (userID: Int, movieID: Int, rating: Int, timestamp: Long)
-
-
-// 1. Create Schema when reading u.data.
+// 1. Create a case class with Schema of u.data.   ~~~DONE~~~
+// 2. Create Schema when reading u.data.   ~~~DONE~~~
 /*
 val userRatingsSchema = new StructType()
-    .add("userID", IntegerType, nullable =true)
+    .add("userID", IntegerType, nullable = true)
     .add("movieID", IntegerType, nullable = true)
     .add("rating", IntegerType, nullable = true)
     .add("timestamp", LongType, nullable = true)
 */
-// 2. Load up the data into a Spark Dataset.
-// 3. Use tab as separator "\t", load Schema from userRatingsSchema and force case class to read
-// it as Dataset.
-// 4. Select only ratings column (The file format is userID, movieID, rating, timestamp).
-// 5. Count up how many times each value (rating) occurs using groupBy and count.
-// 6. Sort the resulting Dataset by count column.
-// 7. Print results from the Dataset.
+// 3. Load up the data into a Spark Dataset.   ~~~DONE~~~
+// 4. Use tab as separator "\t", load Schema from userRatingsSchema and force case class to read
+// it as Dataset.   ~~~DONE~~~
+// 5. Select only ratings column (The file format is userID, movieID, rating, timestamp).   ~~~DONE~~~
+// 6. Count up how many times each value (rating) occurs using groupBy and count.
+// 7. Sort the resulting Dataset by count column.
+// 8. Print results from the Dataset.
 
 
 // Imports.
@@ -36,6 +33,7 @@ import org.apache.spark.sql.types.{ IntegerType, LongType, StructType }
 
 object Homework6 {
 
+    // 1. Create a case class with Schema of u.data.
     // Case class is used to define Schema for our database.
     // Each field corresponds to the column name and column type in our database. This is required
     // to convert DataFrame to Dataset for compile time type check and optimization.
@@ -53,34 +51,53 @@ object Homework6 {
             .getOrCreate()
 
 
-        // 1. Create Schema when reading u.data.
+        // 2. Create Schema when reading u.data.
         val userRatingsSchema = new StructType()
-            .add("userID", IntegerType, nullable =true)
+            .add("userID", IntegerType, nullable = true)
             .add("movieID", IntegerType, nullable = true)
             .add("rating", IntegerType, nullable = true)
             .add("timestamp", LongType, nullable = true)
 
 
-        // 2. Load up the data into a Spark Dataset.
-        //val movieObjectRDD = spark.sparkContext
-        //    .textFile("./assets/ml-100k/u.data")
-        //    .map(x => Movie(x.split("\t")(1).toInt))
-
-
-        // 3. Use tab as separator "\t", load Schema from userRatingsSchema and force case class
+        // 3. Load up the data into a Spark Dataset.
+        // 4. Use tab as separator "\t", load Schema from userRatingsSchema and force case class
         // to read it as Dataset.
+        import spark.implicits._
+        val userRatingsDS = spark.read
+            .option("sep", "\t")
+            .schema(userRatingsSchema)
+            .csv("./assets/ml-100k/u.data")
+            .as[Movie]
 
 
-        // 4. Select only ratings column (The file format is userID, movieID, rating, timestamp).
+        // 5. Select only ratings column (The file format is userID, movieID, rating, timestamp).
+        val ratingsColumn = userRatingsDS.select("rating")
+
+        // 6. Count up how many times each value (rating) occurs using groupBy and count.
+        val ratingsCount = ratingsColumn
+            .groupBy("rating")
+            .count()
 
 
-        // 5. Count up how many times each value (rating) occurs using groupBy and count.
+        // 7. Sort the resulting Dataset by count column.
+        val ratingsCountSorted = ratingsCount.sort("count")
 
+        // 8. Print results from the Dataset.
+        ratingsCountSorted.show(ratingsCountSorted.count.toInt)
 
-        // 6. Sort the resulting Dataset by count column.
+        /* Output:
 
+        +------+-----+
+        |rating|count|
+        +------+-----+
+        |     1| 6110|
+        |     2|11370|
+        |     5|21201|
+        |     3|27145|
+        |     4|34174|
+        +------+-----+
 
-        // 7. Print results from the Dataset.
+        */
 
     }
 }
